4e33ecf88b410055d65f0a54ed123b09951a1390
Jules Tréhorel <jules.trehorel@iadvize.com>
2022-07-13 14:19:40 +0200

    (build) update dependencies

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -8,20 +8,21 @@ buildscript {
     }
 
     dependencies {
-        classpath(BuildDependencies.gms)
-        classpath(BuildDependencies.gradle)
-        classpath(BuildDependencies.kotlin)
+        classpath(Dependencies.Build.gms)
+        classpath(Dependencies.Build.gradle)
+        classpath(Dependencies.Build.kotlin)
     }
 }
 
 allprojects {
     repositories {
         google()
+        gradlePluginPortal()
         maven(url = uri("https://raw.github.com/iadvize/iadvize-android-sdk/master"))
         maven(url = uri("https://plugins.gradle.org/m2/"))
         maven(url = uri("https://jitpack.io"))
         mavenCentral()
-        gradlePluginPortal()
+        mavenLocal()
     }
 }
 
--- sample/buildSrc/src/main/kotlin/Versions.kt
+++ sample/buildSrc/src/main/kotlin/Versions.kt
@@ -4,9 +4,9 @@ object  Versions {
     const val androidxConstraintLayout = "2.1.3"
     const val androidxMultiDex = "2.0.1"
     const val androidxRecyclerView = "1.2.1"
-    const val firebaseBom = "30.0.1"
+    const val firebaseBom = "30.1.0"
     const val gms = "4.3.10"
-    const val gradle = "7.2.0"
-    const val kotlin = "1.6.21"
+    const val gradle = "7.2.1"
+    const val kotlin = "1.7.0"
     const val material = "1.6.0"
 }
